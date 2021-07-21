ce745c9f747a9cbeb5ffa0ac57147af2b226c33f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-07-21 21:42:08 +0200

    Added a sample gui.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello snake.");
+        startGame();
+
+        //Gui Test
+        SnakeGUI textAreaFrame = new SnakeGUI();
+        textAreaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textAreaFrame.setSize(425, 200);
+        textAreaFrame.setVisible(true);
+    }
+
+    public static void startGame(){
         JOptionPane.showMessageDialog(null,"Hey world");
     }
 }
