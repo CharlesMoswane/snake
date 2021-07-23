20c6a59b02243e865261b9a08934e761e59c81d3
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-07-23 22:11:24 +0200

    Added an X label.

--- src/main/java/SnakeGUI.java
+++ src/main/java/SnakeGUI.java
@@ -1,26 +1,30 @@
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
-import javax.swing.JScrollPane;
+import javax.swing.JLabel;
 
 public class SnakeGUI extends JFrame{
 
     private final JTextArea displayArea; // displays demo string
     private final JButton startButton; // initiates copying of text
+    private final JLabel startLabel;
 
     //No argument constructor
     public SnakeGUI(){
         super("Snake");
         Box box = Box.createVerticalBox(); // create box
-        String demo = "This is a demo string to\n" +
-                "illustrate copying text\nfrom one textarea to \n" +
-                "another textarea using an\nexternal event\n";
-        displayArea = new JTextArea(demo, 20, 20);
+        String demo = "Snakes poisson. Pitbulls are the pits.";
+        displayArea = new JTextArea(demo, 40, 40);
         displayArea.setEditable(false);
         box.add(displayArea); // add scrollpane
+
+        startLabel = new JLabel("X");
+        startLabel.setSize(1,1);
+        box.add(startLabel);
 
         startButton = new JButton("Start"); // create copy button
         box.add(startButton); // add copy button to box
@@ -29,12 +33,15 @@ public class SnakeGUI extends JFrame{
                     // set text in textArea2 to selected text from displayArea
                     @Override
                     public void actionPerformed(ActionEvent event) {
-                        //reset screen
-//                        startButton.setText("Stop");
-                        if (startButton.getText() == "Start")
+
+                        if (startButton.getText() == "Start") {
                             startButton.setText("Stop");
-                        else
+                            startLabel.setVisible(true);
+                        }
+                        else {
                             startButton.setText("Start");
+                            startLabel.setVisible(false);
+                        }
                     }
                 });
 
