3b62eaa0b31f4ee4b26c8fb1d7281c29099dbe82
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-02-10 18:00:48 +0200

    Add labels to my board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,10 +1,32 @@
 import javax.swing.*;
 
 public class Board extends JFrame {
+    private JLabel[][] board;
+
     public Board() {
         setTitle("Snake");
         setSize(300, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        board = new JLabel[20][20];
+        drawBoard();
+    }
+
+    public void drawBoard() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new java.awt.GridLayout(20, 20));
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                board[i][j] = new JLabel("x");
+                board[i][j].setOpaque(true);
+                board[i][j].setVisible(true);
+                board[i][j].setBackground(java.awt.Color.WHITE);
+                panel.add(board[i][j]);
+            }
+        }
+
+        add(panel);
     }
 }
