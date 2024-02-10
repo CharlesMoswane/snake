d2a5a86f5ea998c52d8ddf574e18a0c7c66f3f2b
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-02-10 17:50:39 +0200

    feat: Add empty board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,10 @@
-public class Board {
+import javax.swing.*;
+
+public class Board extends JFrame {
+    public Board() {
+        setTitle("Snake");
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
