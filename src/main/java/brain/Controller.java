cffbedfe8f45caeeb2d857f3de03f417c46e00f8
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-03-06 12:45:00 +0200

    feat: Persist network

--- src/main/java/brain/Controller.java
+++ src/main/java/brain/Controller.java
@@ -1,6 +1,9 @@
 package brain;
 
+import data.ModelPersistenceManager;
 import enums.Direction;
+
+import java.io.File;
 
 public class Controller {
     private NeuralNetwork brain;
@@ -8,6 +11,15 @@ public class Controller {
     public Controller(int inputNodes, int hiddenNodes, int outputNodes) {
         brain = new NeuralNetwork(inputNodes, new int[]{hiddenNodes, hiddenNodes, outputNodes},
                                     ActivationFunctions.sigmoid);
+
+        File dir = new File("C:\\Users\\cmosw\\Desktop\\OneDrive\\My Documents\\8. Creation" +
+                            "\\1. My Projects\\1. AI\\1. Snake\\1. Data\\models\\");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        ModelPersistenceManager modelPersistenceManager = new ModelPersistenceManager();
+        modelPersistenceManager.saveModelToJsonFile(brain, dir + "\\model.json");
     }
 
     public Direction getDirection(int[] input) {
