a9832158889e89e5cf1f3e57950df49c69ca06a8
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-03-08 13:15:00 +0200

    feat: Add an arraylist of the layers to the neural network

--- src/main/java/brain/NeuralNetwork.java
+++ src/main/java/brain/NeuralNetwork.java
@@ -1,13 +1,16 @@
 package brain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NeuralNetwork {
     private Layer inputLayer;
     private Layer hiddenLayer;
     private Layer outputLayer;
-    private double learningRate;
 
-    public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes, double learningRate) {
-        this.learningRate = learningRate;
+    private List<Layer> layers = new ArrayList<>();
+
+    public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes) {
         inputLayer = new Layer(inputNodes, 0, (v) -> v);
         hiddenLayer = new Layer(hiddenNodes, inputNodes, (v) -> 1 / (1 + Math.exp(-v)));
         outputLayer = new Layer(outputNodes, hiddenNodes, (v) -> 1 / (1 + Math.exp(-v)));
