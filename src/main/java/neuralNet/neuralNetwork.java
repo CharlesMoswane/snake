c288e1aa57891dd48d1cf0198d7ccc3ad661ab05
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-03-26 13:15:00 +0200

    feat: Add sigmoid function, its derivative and a method to train the network

--- src/main/java/neuralNet/neuralNetwork.java
+++ src/main/java/neuralNet/neuralNetwork.java
@@ -52,4 +52,63 @@ public class neuralNetwork {
 
         return finalOutputs;
     }
+
+    private double sigmoid(double x) {
+        return 1 / (1 + Math.exp(-x));
+    }
+
+    private double sigmoidDerivative(double x) {
+        return x * (1 - x);
+    }
+
+    public void train(double[] inputs, double[] targets) {
+        double[] hiddenOutputs = new double[hiddenNodes];
+        double[] finalOutputs = new double[outputNodes];
+        double[] outputErrors = new double[outputNodes];
+        double[] hiddenErrors = new double[hiddenNodes];
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            double sum = 0;
+            for (int j = 0; j < inputNodes; j++) {
+                sum += weightsInputHidden[i][j] * inputs[j];
+            }
+            hiddenOutputs[i] = sigmoid(sum);
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            double sum = 0;
+            for (int j = 0; j < hiddenNodes; j++) {
+                sum += weightsHiddenOutput[i][j] * hiddenOutputs[j];
+            }
+            finalOutputs[i] = sigmoid(sum);
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            outputErrors[i] = targets[i] - finalOutputs[i];
+        }
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            double sum = 0;
+            for (int j = 0; j < outputNodes; j++) {
+                sum += weightsHiddenOutput[j][i] * outputErrors[j];
+            }
+            hiddenErrors[i] = sum;
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            for (int j = 0; j < hiddenNodes; j++) {
+                double delta = outputErrors[i] * sigmoidDerivative(finalOutputs[i]) * hiddenOutputs[j];
+                weightsHiddenOutput[i][j] += learningRate * delta;
+            }
+        }
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            for (int j = 0; j < inputNodes; j++) {
+                double delta = hiddenErrors[i] * sigmoidDerivative(hiddenOutputs[i]) * inputs[j];
+                weightsInputHidden[i][j] += learningRate * delta;
+            }
+        }
+    }
+
+
 }
