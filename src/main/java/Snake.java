d2d26644c4132445c8c80b5e19bfa68151a5b558
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-01 13:15:00 +0200

    feat: Refactored snake constructor

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -2,13 +2,12 @@ import java.util.LinkedList;
 
 public class Snake {
 
-    private LinkedList<SnakeComponent> snakeBody;
+    private final LinkedList<SnakeComponent> snakeBody;
     private Direction direction;
 
     public Snake(int x, int y, int snakeLength) {
         snakeBody = new LinkedList<>();
-        snakeBody.add(new SnakeComponent(x, y));
-        for (int i = 1; i < snakeLength; i++) {
+        for (int i = 0; i < snakeLength; i++) {
             snakeBody.add(new SnakeComponent(x, y - i));
         }
         this.direction = Direction.RIGHT;
